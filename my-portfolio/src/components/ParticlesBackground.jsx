3185ed4cfa2f2edce32e794269bc0bb868d4ec26
Ivy Xu <xyan21418@gmail.com>
2024-11-27 17:49:59 +0200

    imported particles config statically

--- my-portfolio/src/components/ParticlesBackground.jsx
+++ my-portfolio/src/components/ParticlesBackground.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react"
 import Particles, { initParticlesEngine } from "@tsparticles/react"
 import { loadFull } from "tsparticles"
+import particlesConfig from '../config/particles.json'
 
 const ParticlesBackground = () => {
   const [init, setInit] = useState(false)
-  const [particleOptions, setParticleOptions] = useState(null)
 
   useEffect(() => {
     const initializeParticles = async () => {
@@ -21,26 +21,12 @@ const ParticlesBackground = () => {
     initializeParticles()
   }, [])
 
-  useEffect(() => {
-    const loadParticlesConfig = async () => {
-      try {
-        const response = await fetch("/particles.json")
-        const data = await response.json()
-        setParticleOptions(data)
-      } catch (error) {
-        console.error("Failed to load particles.json:", error)
-      }
-    }
-
-    loadParticlesConfig()
-  }, [])
-
   return (
     <div>
       {init && (
         <Particles
           id="tsparticles"
-          options={particleOptions}
+          options={particlesConfig}
         />
       )}
     </div>
