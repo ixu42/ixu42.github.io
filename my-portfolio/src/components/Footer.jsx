08b3708603913e1063834737a6009b3d2d9155ab
Ivy Xu <xyan21418@gmail.com>
2024-11-27 18:58:58 +0200

    implemented footer with social media links

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -1,5 +1,21 @@
+import { FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa'
+
 const Footer = () => {
-  
+  return (
+    <footer className='footer'>
+      <div className="social-links">
+        <a href="mailto:xyan21418@gmail.com" target="_blank" rel="noopener noreferrer">
+          <FaEnvelope />
+        </a>
+        <a href="https://github.com/ixu42" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://linkedin.com/in/ixu42" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
